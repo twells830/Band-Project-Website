db8a1b111c75121b7e2d48d8d086c55f2f0993ac
Tanner Wells <twells830@gmail.com>
2015-08-13 19:13:31 -0400

    added resizable text/buttons for xs

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 function elResize(){
 var width = $(window).width();
-	if(width <992){
+	if(width < 992 && width > 768){
+		//small devices 768px - 992px
 		$(".bandName").css("font-size", "50px");
 		$("button").css("font-size", "16px");
 		$("button").css("padding", "20px");
+	}else if(width < 768){
+		//extra small devices 0px - 768px
+		$(".bandName").css("font-size", "25px");
+		$("button").css("font-size", "12px");
+		$("button").css("padding", "16px");
 	}else{
 		$(".bandName").css("font-size", "100px");
 		$("button").css("font-size", "28px");
 		$("button").css("padding", "30px");
-
-
 	}
 };
 
