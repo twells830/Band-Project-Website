7530c8cd7b1e96391c78dbab367c06d3bfffd441
Tanner Wells <twells830@gmail.com>
2015-08-17 17:18:07 -0400

    textBlocks
    textBlocks hide and show depending on nav bar

--- js/script.js
+++ js/script.js
@@ -1,35 +1,45 @@
 function elResize(){
-var width = $(window).width();
-	if(width < 992 && width > 768){
-		//small devices 768px - 992px
-		$(".bandName").css("font-size", "50px");
-		$(".bandName").css("color", "black");
-		$("button").css("font-size", "16px");
-		$("button").css("padding", "20px");
-		$(".backgroundVideo").hide();
-		$(".altBack").show();
 
-	}else if(width < 768){
-		//extra small devices 0px - 768px
-		$(".bandName").css("font-size", "25px");
-		$(".bandName").css("color", "black");
-		$("button").css("font-size", "12px");
-		$("button").css("padding", "16px");
-		$(".backgroundVideo").hide();
-		$(".altBack").show();
+	var width = $(window).width();
 
+		if(width < 992 && width > 768){
+			//small devices 768px - 992px
+			$(".bandName").css("font-size", "50px");
+			$(".bandName").css("color", "black");
+			$("button").css("font-size", "16px");
+			$("button").css("padding", "20px");
+			$(".backgroundVideo").hide();
+			$(".altBack").show();
 
-	}else{
-		$(".bandName").css("font-size", "100px");
-		$(".bandName").css("color", "#f6f6f6");
-		$("button").css("font-size", "28px");
-		$("button").css("padding", "30px");
-		$(".altBack").hide();
-		$(".backgroundVideo").show();
+		}else if(width < 768){
+			//extra small devices 0px - 768px
+			$(".bandName").css("font-size", "25px");
+			$(".bandName").css("color", "black");
+			$("button").css("font-size", "12px");
+			$("button").css("padding", "16px");
+			$(".backgroundVideo").hide();
+			$(".altBack").show();
 
-	}
+		}else{
+			$(".bandName").css("font-size", "100px");
+			$(".bandName").css("color", "#f6f6f6");
+			$("button").css("font-size", "28px");
+			$("button").css("padding", "30px");
+			$(".altBack").hide();
+			$(".backgroundVideo").show();
+
+		}
 };
 
+$(function() {
+    $('.navLink').on('click', function() {
+        $('#'+$(this).data('id')).show().siblings('.textBlock').hide();
+    });
+});
+
+$(document).ready(function(){
+	$(".page .textBlock").hide()
+	$("#gigs").show();
+});
 $(document).ready(elResize);
 $(window).resize(elResize);
-
