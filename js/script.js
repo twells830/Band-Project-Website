b8fe51325848e816f49eff3a6c089f74c8d715fe
Tanner Wells <tannerjameswells@gmail.com>
2016-09-29 13:15:14 -0400

    refactoring/redesigning this whole damn site

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-//for making things appear on scroll
-// $(document).scroll(function() {
-//   var y = $(this).scrollTop();
-//   if (y > 800) {
-//     $('.bottomMenu').fadeIn();
-//   } else {
-//     $('.bottomMenu').fadeOut();
-//   }
-// });
+$(window).scroll(function(){
+  var wScroll = $(this).scrollTop();
+  $('.logo').css({
+    'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+  });
+  $('.back-bird').css({
+    'transform' : 'translate(0px, '+ wScroll /4 +'%)'
+  })
+  $('.fore-bird').css({
+    'transform' : 'translate(0px, -'+ wScroll /20 +'%)'
+  });
+
+//clothes-pics container for whatever you want to land
+  if (wScroll > $('.clothes-pics').offset().top - ($(window).height() * 0.1)) {
+    $('.clothes-pics figure').each(function(i) {
+      setTimeout(function() {
+        $('.clothes-pics figure').eq(i).addClass('is-showing');
+      }, 500 * (i+1));
+    });
+  }
+});
